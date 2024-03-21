759c287097d12d88b3a68b0462412756b65ed213
CodeNova-Kumar <KUMARVERMA2002@GMAIL.COM>
2024-03-21 20:16:28 +0530

    LoginSignup K

--- Hover/src/App.jsx
+++ Hover/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import LoginPAge from './Components/LoginPAge.jsx'
 import About from './Components/About'
 import './App.css'
-import Login from './Components/Login'
 import Signup from './Components/Signup'
 import Home from './Components/Home'
+// import LoginPAge from './Components/LoginPAge'
 
 const App = () => {
   return (
@@ -14,7 +15,7 @@ const App = () => {
         <Navbar />
         <Routes>
             <Route path="/About" element={<About />} />
-            <Route path="/Login" element={<Login />} />
+            <Route path="/Login" element={<LoginPAge />} />
             <Route path="/Signup" element={<Signup />} />
             <Route path="/Home" element={<Home />} />
             </Routes>
